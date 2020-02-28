01a38a82fe24b6f4825128a845ee11d0a6d236ae
kplee <kplee@cern.ch>
2020-02-28 13:07:07 +0900

    py script for hadd command

---
+++ NtupleAnalyzer/RateAndPurity/runFR/pyscript_hadd_outputs.py
@@ -0,0 +1,25 @@
+import os
+
+# -- settings -- #
+scratchPath = "/d0/scratch/kplee"
+
+def RunHadd(dataset, version, trigger, etaMin = "0.0", etaMax = "5.0"):
+
+    outputName = "./Output-MuonTriggerPurity-{dataset_}-{version_}-{trigger_}-{etaMin_}-{etaMax_}.root".format(
+        dataset_=dataset, version_=version, trigger_=trigger, etaMin_=etaMin, etaMax_=etaMax)
+
+    batchDirPath = "{scratchPath_}/BatchDir_{version_}/{dataset_}/{trigger_}/Job*/Output*.root".format(
+        scratchPath_=scratchPath, version_=version, dataset_=dataset, trigger_=trigger)
+
+    cmd_hadd = """
+    hadd {outputName_} \\
+    {batchDirPath_}""".format(outputName_=outputName, batchDirPath_=batchDirPath)
+
+    print cmd_hadd
+    os.system(cmd_hadd)
+    print "[Done]\n"
+
+
+if __name__ == '__main__':
+    RunHadd( "TTSemiLep80X", "v01", "hltL2fL1sMu22L1f0L2Filtered10Q" )
+    RunHadd( "TTSemiLep102X", "v01", "hltL2fL1sMu22L1f0L2Filtered10Q" )
