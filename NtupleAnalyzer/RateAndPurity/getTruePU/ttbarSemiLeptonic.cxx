1921755156ea6884adc8b07b029b594abd09341f
kplee <kplee@cern.ch>
2020-03-06 14:01:21 +0900

    fix typo

--- NtupleAnalyzer/RateAndPurity/getTruePU/ttbarSemiLeptonic.cxx
+++ NtupleAnalyzer/RateAndPurity/getTruePU/ttbarSemiLeptonic.cxx
@@ -3,7 +3,7 @@
 void ttbarSemiLeptonic()
 {
   vector<TString> vec_dataPath = {
-    "/pnfs/knu.ac.kr/data/cms/store/user/moh/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/crab_MuonHLTNtuple_SimMatchingInfo_2018_TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/191124_074614/0000/ntuple_*.root";
+    "/pnfs/knu.ac.kr/data/cms/store/user/moh/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/crab_MuonHLTNtuple_SimMatchingInfo_2018_TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/191124_074614/0000/ntuple_*.root"
   };
 
   HistProducer *producer = new HistProducer("ttbarSemiLeptonic_102X");
