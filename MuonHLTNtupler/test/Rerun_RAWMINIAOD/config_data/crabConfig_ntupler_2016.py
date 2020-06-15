7395fa5c9a96c71a5c2b4b718978b8a5c1543c7f
Kyeongpil Lee <kplee@cern.ch>
2020-06-15 20:48:17 +0900

    crab submission codes

---
+++ MuonHLTNtupler/test/Rerun_RAWMINIAOD/config_data/crabConfig_ntupler_2016.py
@@ -0,0 +1,46 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = ''
+config.General.workArea = 'CRABDir'
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = ''
+config.JobType.numCores = 4
+# config.JobType.maxMemoryMB = 2500
+# config.JobType.maxJobRuntimeMin = 2000
+
+config.Data.inputDataset = ''
+# config.Data.useParent = True
+
+config.Data.inputDBS = 'global'
+config.Data.publication = False
+
+# config.Data.splitting = 'Automatic' # -- it is not preferred: test job is frequently failed due to memory
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 5
+
+config.Data.outLFNDirBase = '/store/user/kplee/'
+config.Site.storageSite = 'T2_KR_KNU'
+
+config.Data.lumiMask = './Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON.txt'
+
+version = '_v20200615_'
+
+# 'MultiCRAB' part
+if __name__ == '__main__':
+    
+    from CRABAPI.RawCommand import crabCommand
+
+    config.General.requestName = 'MuonHLTNtuple'+version+'HLTPhysics_Run2016Hv2_Rerun'
+    config.Data.inputDataset = '/HLTPhysics/Run2016H-PromptReco-v2/MINIAOD'
+    config.Data.secondaryInputDataset = '/HLTPhysics/Run2016H-v1/RAW'
+    config.JobType.psetName = 'HLTCfg_2016_Data.py'
+    crabCommand('submit', config = config)
+
+    config.General.requestName = 'MuonHLTNtuple'+version+'HLTPhysics_Run2016Hv3_Rerun'
+    config.Data.inputDataset = '/HLTPhysics/Run2016H-PromptReco-v3/MINIAOD'
+    config.Data.secondaryInputDataset = '/HLTPhysics/Run2016H-v1/RAW'
+    config.JobType.psetName = 'HLTCfg_2016_Data.py'
+    crabCommand('submit', config = config)
+
---
+++ MuonHLTNtupler/test/Rerun_RAWMINIAOD/config_data/crabConfig_ntupler_2018.py
@@ -0,0 +1,41 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = ''
+config.General.workArea = 'CRABDir'
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = ''
+config.JobType.numCores = 4
+# config.JobType.maxMemoryMB = 2500
+# config.JobType.maxJobRuntimeMin = 2000
+
+config.Data.inputDataset = ''
+# config.Data.useParent = True
+
+config.Data.inputDBS = 'global'
+config.Data.publication = False
+
+# config.Data.splitting = 'Automatic' # -- it is not preferred: test job is frequently failed due to memory
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 5
+
+config.Data.outLFNDirBase = '/store/user/kplee/'
+config.Site.storageSite = 'T2_KR_KNU'
+
+config.Data.lumiMask = './Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt'
+
+version = '_v20200615_'
+
+# 'MultiCRAB' part
+if __name__ == '__main__':
+    
+    from CRABAPI.RawCommand import crabCommand
+
+    # -- for efficiency: useParent = True
+    config.General.requestName = 'MuonHLTNtuple'+version+'HLTPhysics_Run2018D_Rerun'
+    config.Data.inputDataset = '/HLTPhysics/Run2018D-PromptReco-v2/MINIAOD'
+    config.Data.secondaryInputDataset = '/HLTPhysics/Run2018D-v1/RAW'
+    config.JobType.psetName = 'HLTCfg_2018_Data.py'
+    crabCommand('submit', config = config)
+
