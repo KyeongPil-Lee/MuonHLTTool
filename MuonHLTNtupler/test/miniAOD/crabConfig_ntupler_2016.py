7889d9b74d295a2b3a390ef29adec44fbe82a3f0
Kyeongpil Lee <kplee@cern.ch>
2020-07-08 00:23:56 +0900

    submit miniAOD jobs

---
+++ MuonHLTNtupler/test/miniAOD/crabConfig_ntupler_2016.py
@@ -0,0 +1,42 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = ''
+config.General.workArea = 'CRABDir'
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = ''
+# config.JobType.numCores = 4
+# config.JobType.maxMemoryMB = 3000
+# config.JobType.maxJobRuntimeMin = 2000
+
+config.Data.inputDataset = ''
+# config.Data.useParent = True
+
+config.Data.inputDBS = 'global'
+config.Data.publication = False
+
+# config.Data.splitting = 'Automatic' # -- it is not preferred: test job is frequently failed due to memory
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 20
+
+config.Data.outLFNDirBase = '/store/user/kplee/'
+config.Site.storageSite = 'T2_KR_KNU'
+
+config.Data.lumiMask = './Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON_Last5invfb.txt'
+
+# config.Data.ignoreLocality = True
+# config.Site.whitelist = ['T2_KR_*', 'T3_KR_*', 'T2_US_*']
+
+version = '_v20200708_'
+
+# 'MultiCRAB' part
+if __name__ == '__main__':
+    
+    from CRABAPI.RawCommand import crabCommand
+
+    config.General.requestName = 'MuonHLTNtuple'+version+'DoubleMuon_07Aug17_Run2016Hv2_Rerun'
+    config.Data.inputDataset = '/DoubleMuon/Run2016H-07Aug17-v1/MINIAOD'
+    config.JobType.psetName = 'Run_ntupler_2016.py'
+    crabCommand('submit', config = config)
+
