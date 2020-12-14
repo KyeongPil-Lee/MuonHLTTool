f4156aee90db4e0525abd2953afaf365a7aa9a4c
kplee <kplee@cern.ch>
2020-12-14 15:26:05 +0100

    fileBased = 1 due to memeory problem

--- MuonHLTNtupler/test/ntupleMaker_IDIso/RerunHLT_RAWMINIAOD/crabconfig_MC.py
+++ MuonHLTNtupler/test/ntupleMaker_IDIso/RerunHLT_RAWMINIAOD/crabconfig_MC.py
@@ -18,7 +18,7 @@ config.Data.publication = False
 
 # config.Data.splitting = 'Automatic' # -- it is not preferred: test job is frequently failed due to memory
 config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 5
+config.Data.unitsPerJob = 1
 
 config.Site.storageSite = 'T3_KR_KNU'
 
@@ -26,7 +26,7 @@ config.Site.storageSite = 'T3_KR_KNU'
 
 config.JobType.allowUndistributedCMSSW = True
 
-version = 'v01'
+version = 'v02'
 config.Data.outLFNDirBase = '/store/user/kplee/MuonHLTTree_%s' % version
 
 # 'MultiCRAB' part
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     config.Data.secondaryInputDataset = '/DYToLL_M-50_TuneCP5_14TeV-pythia8_HCAL/Run3Winter20DRPremixMiniAOD-110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-DIGI-RAW'
     crabCommand('submit', config = config)
 
-    # config.General.requestName = 'QCD_Pt15to7000_MuEnriched_Flat_Pythia8'
-    # config.Data.inputDataset = '/QCD_Pt_15to7000_TuneCP5_muEnriched_Flat_14TeV_pythia8/Run3Winter20DRMiniAOD-FlatPU0to80ALCARECO_110X_mcRun3_2021_realistic_v6-v2/MINIAODSIM'
-    # config.Data.secondaryInputDataset = '/QCD_Pt_15to7000_TuneCP5_muEnriched_Flat_14TeV_pythia8/Run3Winter20DRMiniAOD-FlatPU0to80ALCARECO_110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-DIGI-RAW'
-    # crabCommand('submit', config = config)
+    config.General.requestName = 'QCD_Pt15to7000_MuEnriched_Flat_Pythia8'
+    config.Data.inputDataset = '/QCD_Pt_15to7000_TuneCP5_muEnriched_Flat_14TeV_pythia8/Run3Winter20DRMiniAOD-FlatPU0to80ALCARECO_110X_mcRun3_2021_realistic_v6-v2/MINIAODSIM'
+    config.Data.secondaryInputDataset = '/QCD_Pt_15to7000_TuneCP5_muEnriched_Flat_14TeV_pythia8/Run3Winter20DRMiniAOD-FlatPU0to80ALCARECO_110X_mcRun3_2021_realistic_v6-v2/GEN-SIM-DIGI-RAW'
+    crabCommand('submit', config = config)
