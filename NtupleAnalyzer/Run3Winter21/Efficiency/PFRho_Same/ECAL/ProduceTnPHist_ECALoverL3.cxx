3f00a2ae9cd0c30e4c4e88a93794dd02f26ab121
Kyeongpil Lee <kplee@Kyeongpils-MacBook-Pro.local>
2021-11-08 19:09:51 +0100

    efficiency for same rho

---
+++ NtupleAnalyzer/Run3Winter21/Efficiency/PFRho_Same/ECAL/ProduceTnPHist_ECALoverL3.cxx
@@ -0,0 +1,16 @@
+#include <Run3Winter21/Efficiency/TnPHistProducer_ECALoverL3.h>
+
+void ProduceTnPHist_ECALoverL3(TString textFile_ntupleList)
+{
+  TString tag = textFile_ntupleList.Data();
+  tag = gSystem->BaseName(tag);
+  tag.ReplaceAll(".txt", "");
+  TString outputFileName = TString::Format("ROOTFile_TnPHist_%s.root", tag.Data());
+
+  HistProducer* histProducer = new HistProducer();
+  histProducer->Set_OutputFileName(outputFileName);
+  histProducer->Set_ntupleListFile(textFile_ntupleList);
+  histProducer->Set_minPt( 26 ); // -- min pT applied for eta, phi and vtx
+
+  histProducer->Produce();
+}
