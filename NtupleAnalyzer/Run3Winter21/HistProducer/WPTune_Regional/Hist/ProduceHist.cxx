9ea8a5428c03e6e322f792c73a7219d0570b90cb
Kyeongpil Lee <kplee@kyeongpils-mbp.home>
2021-12-10 16:27:19 +0100

    update new WP for ECAL

--- NtupleAnalyzer/Run3Winter21/HistProducer/WPTune_Regional/Hist/ProduceHist.cxx
+++ NtupleAnalyzer/Run3Winter21/HistProducer/WPTune_Regional/Hist/ProduceHist.cxx
@@ -14,7 +14,7 @@ void ProduceHist(TString textFile_ntupleList)
   histProducer->SetRange_PU_LowPU( 0, 40 );
   histProducer->SetRange_PU_HighPU( 40, 60 );
 
-  histProducer->Set_NewWP_ECAL( 0.23210, 0.19400 ); // -- 99% singal efficiency in high PU
+  histProducer->Set_NewWP_ECAL( 0.23200, 0.19400 ); // -- 99% singal efficiency in high PU
   histProducer->Set_NewWP_HCAL( 0.16, 0.20 ); // -- default value for now
 
   histProducer->Produce();
