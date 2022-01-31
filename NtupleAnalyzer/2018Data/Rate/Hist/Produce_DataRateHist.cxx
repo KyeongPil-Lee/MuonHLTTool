2c8705c16e8757b9193b2ff2e30e9cf9515f5d56
Kyeongpil Lee <kplee@cern.ch>
2022-01-31 16:44:46 +0100

    result

--- NtupleAnalyzer/2018Data/Rate/Hist/Produce_DataRateHist.cxx
+++ NtupleAnalyzer/2018Data/Rate/Hist/Produce_DataRateHist.cxx
@@ -7,9 +7,14 @@ void Produce_DataRateHist(TString textFile_ntupleList) {
   producer->Set_TriggerList_HLT( {"IsoMu24_v", "Mu50_v"} );
   producer->Set_TriggerList_MYHLT( {"IsoMu24_v", "Mu50_v"} );
 
+  // -- when you run over just 1 HLT physics dataset
+  // Double_t prescale_HLTPhysics = 1100;
+  // Double_t nHLTPhysicsDataset = 8;
+  // producer->Set_Weight( prescale_HLTPhysics*nHLTPhysicsDataset );
+
+  // -- when you run over full HLTPhysics dataset (total 8 datasets)
   Double_t prescale_HLTPhysics = 1100;
-  Double_t nHLTPhysicsDataset = 8;
-  producer->Set_Weight( prescale_HLTPhysics*nHLTPhysicsDataset );
+  producer->Set_Weight( prescale_HLTPhysics );
 
   producer->Produce();
 }
--- NtupleAnalyzer/2018Data/Rate/Hist/pyscript_condorJobSubmit.py
+++ NtupleAnalyzer/2018Data/Rate/Hist/pyscript_condorJobSubmit.py
@@ -6,8 +6,8 @@ submitter.ROOTCodeName = "Produce_DataRateHist.cxx"
 
 submitter.sampleInfoYAML = "sampleInfo_v3.yml" # -- $MUONHLT_ANALYZER_PATH/Include
 submitter.dic_sample_nJob = {
-    "EphemeralHLTPhysics1_Run2018D_Run323775_withoutPT": 5,
-    "EphemeralHLTPhysics1_Run2018D_Run323775": 5,
+    "EphemeralHLTPhysicsAll_Run2018D_Run323775_withoutPT": 20,
+    "EphemeralHLTPhysicsAll_Run2018D_Run323775": 20,
 }
 
 submitter.doMergeBinnedSample = True
