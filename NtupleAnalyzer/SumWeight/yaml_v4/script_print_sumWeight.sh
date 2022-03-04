2f9128c8d3bbdb03976bbff4866b3ecd461a3985
Kyeongpil Lee <kplee@kyeongpils-mbp.home>
2022-03-04 13:24:55 +0100

    update sum(weight)

--- NtupleAnalyzer/SumWeight/yaml_v4/script_print_sumWeight.sh
+++ NtupleAnalyzer/SumWeight/yaml_v4/script_print_sumWeight.sh
@@ -35,4 +35,76 @@ root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt50to80_nVtx_nRegion_4_
 root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt80to120_nVtx_nRegion_4_40.root Print_SumWeight.cxx
 root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt120to170_nVtx_nRegion_4_40.root Print_SumWeight.cxx
 
+root -l -b -q ROOTFile_MakeHist_SumWeight_ZMuMu_M50to120_nVtx_nRegion_6_60.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt20to30_nVtx_nRegion_6_60.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt30to50_nVtx_nRegion_6_60.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt50to80_nVtx_nRegion_6_60.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt80to120_nVtx_nRegion_6_60.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt120to170_nVtx_nRegion_6_60.root Print_SumWeight.cxx
+
+root -l -b -q ROOTFile_MakeHist_SumWeight_ZMuMu_M50to120_nVtx_nRegion_8_80.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt20to30_nVtx_nRegion_8_80.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt30to50_nVtx_nRegion_8_80.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt50to80_nVtx_nRegion_8_80.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt80to120_nVtx_nRegion_8_80.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt120to170_nVtx_nRegion_8_80.root Print_SumWeight.cxx
+
+root -l -b -q ROOTFile_MakeHist_SumWeight_ZMuMu_M50to120_nVtx_nRegion_10_100.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt20to30_nVtx_nRegion_10_100.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt30to50_nVtx_nRegion_10_100.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt50to80_nVtx_nRegion_10_100.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt80to120_nVtx_nRegion_10_100.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt120to170_nVtx_nRegion_10_100.root Print_SumWeight.cxx
+
+root -l -b -q ROOTFile_MakeHist_SumWeight_ZMuMu_M50to120_nVtx_nRegion_20_200.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt20to30_nVtx_nRegion_20_200.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt30to50_nVtx_nRegion_20_200.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt50to80_nVtx_nRegion_20_200.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt80to120_nVtx_nRegion_20_200.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt120to170_nVtx_nRegion_20_200.root Print_SumWeight.cxx
+
+
+root -l -b -q ROOTFile_MakeHist_SumWeight_ZMuMu_M50to120_newParam_nVtx_nRegion_1_10.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt20to30_newParam_nVtx_nRegion_1_10.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt30to50_newParam_nVtx_nRegion_1_10.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt50to80_newParam_nVtx_nRegion_1_10.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt80to120_newParam_nVtx_nRegion_1_10.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt120to170_newParam_nVtx_nRegion_1_10.root Print_SumWeight.cxx
+
+root -l -b -q ROOTFile_MakeHist_SumWeight_ZMuMu_M50to120_newParam_nVtx_nRegion_2_20.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt20to30_newParam_nVtx_nRegion_2_20.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt30to50_newParam_nVtx_nRegion_2_20.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt50to80_newParam_nVtx_nRegion_2_20.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt80to120_newParam_nVtx_nRegion_2_20.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt120to170_newParam_nVtx_nRegion_2_20.root Print_SumWeight.cxx
+
+root -l -b -q ROOTFile_MakeHist_SumWeight_ZMuMu_M50to120_newParam_nVtx_nRegion_3_30.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt20to30_newParam_nVtx_nRegion_3_30.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt30to50_newParam_nVtx_nRegion_3_30.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt50to80_newParam_nVtx_nRegion_3_30.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt80to120_newParam_nVtx_nRegion_3_30.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt120to170_newParam_nVtx_nRegion_3_30.root Print_SumWeight.cxx
+
+root -l -b -q ROOTFile_MakeHist_SumWeight_ZMuMu_M50to120_newParam_nVtx_nRegion_4_40.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt20to30_newParam_nVtx_nRegion_4_40.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt30to50_newParam_nVtx_nRegion_4_40.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt50to80_newParam_nVtx_nRegion_4_40.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt80to120_newParam_nVtx_nRegion_4_40.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt120to170_newParam_nVtx_nRegion_4_40.root Print_SumWeight.cxx
+
+root -l -b -q ROOTFile_MakeHist_SumWeight_ZMuMu_M50to120_newParam_nVtx_nRegion_5_50.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt20to30_newParam_nVtx_nRegion_5_50.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt30to50_newParam_nVtx_nRegion_5_50.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt50to80_newParam_nVtx_nRegion_5_50.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt80to120_newParam_nVtx_nRegion_5_50.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt120to170_newParam_nVtx_nRegion_5_50.root Print_SumWeight.cxx
+
+
+root -l -b -q ROOTFile_MakeHist_SumWeight_ZMuMu_M50to120_woPatatrack.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt20to30_woPatatrack.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt30to50_woPatatrack.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt50to80_woPatatrack.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt80to120_woPatatrack.root Print_SumWeight.cxx
+root -l -b -q ROOTFile_MakeHist_SumWeight_QCDMuEnriched_Pt120to170_woPatatrack.root Print_SumWeight.cxx
+
 echo "finished"
