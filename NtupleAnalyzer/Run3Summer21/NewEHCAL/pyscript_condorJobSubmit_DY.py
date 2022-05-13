530ac0aa855592b4da0cf5762c1180804d2dd515
Kyeongpil Lee <kplee@Kyeongpils-MacBook-Pro.local>
2022-05-13 13:37:52 +0200

    process nominal one also

--- NtupleAnalyzer/Run3Summer21/NewEHCAL/pyscript_condorJobSubmit_DY.py
+++ NtupleAnalyzer/Run3Summer21/NewEHCAL/pyscript_condorJobSubmit_DY.py
@@ -6,8 +6,8 @@ submitter.ROOTCodeName = "ProduceHist.cxx"
 
 submitter.sampleInfoYAML = "sampleInfo_v5.yml" # -- $MUONHLT_ANALYZER_PATH/Include
 submitter.dic_sample_nJob = {
-    # "ZMuMu_M50to120": 10,
-    "ZMuMu_M50to120_newEHCAL": 10,
+    "ZMuMu_M50to120": 20,
+    "ZMuMu_M50to120_newEHCAL": 20,
 }
 
 submitter.doMergeBinnedSample = True
