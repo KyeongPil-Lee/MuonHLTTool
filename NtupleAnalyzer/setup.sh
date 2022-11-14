f7071adca2b8b92ab696796e4996e59ee3fea4e8
Kyeongpil Lee <kplee@kyeongpils-mbp.home>
2022-11-14 10:50:41 +0100

    setup for T2 IIHE

--- NtupleAnalyzer/setup.sh
+++ NtupleAnalyzer/setup.sh
@@ -7,6 +7,12 @@ fi
 
 export MUONHLT_ANALYZER_PATH=$(pwd)
 
+if [[ "$HOSTNAME" == *iihe.ac.be ]]; then
+  echo "IIHE T2: setup LCG100 Env."
+  source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_100 x86_64-centos7-gcc10-opt
+  export LD_PRELOAD=/usr/lib64/libopenblas.so.0
+fi
+
 # -- root setup -- #
 export ROOT_INCLUDE_PATH=${MUONHLT_ANALYZER_PATH}:${ROOT_INCLUDE_PATH}
 export PYTHONPATH=${MUONHLT_ANALYZER_PATH}:${PYTHONPATH}
